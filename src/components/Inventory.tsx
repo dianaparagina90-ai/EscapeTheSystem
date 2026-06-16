a156db983938383d0b63baa367e8064a0f54b480
Diana Paragina <diana.paragina90@gmail.com>
2026-06-16 09:33:01 +0200

    styling with tailwind and local state for rooms

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -8,16 +8,22 @@ const Inventory = ({ onItemClick }: IProps) => {
   const { inventory } = useInventory();
 
   return (
-    <div className="inventory">
-      {inventory.map((i) => (
-        <img
-          key={i.id}
-          src={i.image}
-          alt={i.description}
-          onClick={() => onItemClick(i.id)}
-        />
-      ))}
-    </div>
+    <>
+      <h4 className="text-xl font-bold text-center">Inventory</h4>
+      <div
+        /*className="inventory"*/ className="flex gap-2 flex-wrap justify-center"
+      >
+        {inventory.map((i) => (
+          <img
+            className="w-30 h-30 object-contain cursor-pointer hover:scale-110 "
+            key={i.id}
+            src={i.image}
+            alt={i.description}
+            onClick={() => onItemClick(i.id)}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,47 @@
 import { Link } from "react-router-dom";
 import rooms from "../data/rooms.json";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [showRooms, setShowRooms] = useState(false);
+
   return (
-    <div className="navbar">
-      <Link to="/">Home</Link>
-      <nav className="roomNavigation">
-        {rooms.map((room) => (
-          <Link key={room.id} to={room.roomPath}>
-            {room.roomName}
-          </Link>
-        ))}
-      </nav>
+    <div
+      className=" flex flex-col gap-4 p-4 bg-gray-950 text-white
+      w-full"
+    >
+      <header className=" relative flex items-center">
+        <Link
+          to="/"
+          className="font-bold hover:text-emerald-600 hover:scale-110 hover:italic"
+        >
+          Home
+        </Link>
+        <h1 className="absolute left-2/5 text-4xl font-bold italic">
+          Escape the System
+        </h1>
+      </header>
+      <div className="flex gap-4 p-2 justify-center items-center">
+        <h3
+          className="text-xl font-bold cursor-pointer hover:text-emerald-600 hover:scale-110 hover:italic"
+          onClick={() => setShowRooms(!showRooms)}
+        >
+          {showRooms ? "Hide Rooms" : "Rooms"}
+        </h3>
+        {showRooms && (
+          <nav className="flex ml-7">
+            {rooms.map((room) => (
+              <Link
+                className="flex gap-5 p-2 hover:italic hover:font-bold hover:text-emerald-600 hover:scale-110 "
+                key={room.id}
+                to={room.roomPath}
+              >
+                {room.roomName}
+              </Link>
+            ))}
+          </nav>
+        )}
+      </div>
     </div>
   );
 };
